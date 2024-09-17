7126df46fcab703efb6234866955ba7c481c147a
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2024-09-17 10:43:07 -0300

    cria cartão

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula da Relação Fundamental da Trigonometria?',
+    'sen² θ + cos² θ = 1');
+    
+    criaCartao(
+    'Química',
+    'Qual é a fórmula geral da nomenclatura de um hidrácido?',
+    'Ácido + nome do elemento + ídrico');
+    
+    criaCartao(
+    'Física',
+    'Quais são as duas fórmulas do Movimento Uniforme?',
+    'Para calcular a posição: S(t) = S₀ + v.t e para calcular a velocidade média: v = ΔS/Δt');
+    
+    criaCartao(
+    'Física',
+    'O que a Primeira Lei de Newton estabelece?',
+    'A primeira lei de Newton, também conhecida como lei da inércia, estabelece que, se a força resultante sobre um corpo for nula (igual a zero), esse corpo estará em repouso ou em movimento retilíneo uniforme.');
+    
+    criaCartao(
+    'História',
+    'O que foi a suserania e a vassalagem?',
+    'As relações de suserania e vassalagem, comuns no feudalismo durante a Idade Média, foram compromissos de fidelidade entre nobres originadas a partir da doação de terras. Quando um nobre doava terras a outro, o que doou se torna suserano e o que recebeu, vassalo. Esse acordo implicava direitos e obrigações recíprocas.');
+    
+    criaCartao(
+    'História',
+    'Quais países faziam parte da Tríplice Entente?',
+    'França, Inglaterra e Rússia.');
+    
+    criaCartao(
+    'História',
+    'Quais países faziam parte da Tríplice Aliança?',
+    'Alemanha, Itália e Império Austro-Húngaro.');
+    
+    criaCartao(
+    'História',
+    'Qual foi o estopim para a Segunda Guerra Mundial?',
+    'O estopim para o conflito deu-se com a invasão da Polônia realizada pelos alemães, em setembro de 1939. ');
+    
+    criaCartao(
+    'Matemática',
+    'Qual é a fórmula para calcular os juros compostos?',
+    'M = C (1+i)^n');
+    
+    criaCartao(
+    'Física',
+    'Qual é a equação fundamental da Ondulatória?',
+    'V = = λ . f');
+    
+    criaCartao(
+    'Física',
+    'Qual é a fórmula da Primeira Lei da Termodinâmica?',
+    'ΔU = Q - W');
+    
+    criaCartao(
+    'Biologia',
+    'O que são genes alelos?',
+    'Os genes alelos são aqueles que ocupam o mesmo lócus em cromossomos homólogos e estão envolvidos na determinação de um mesmo caráter. ');
+    
+    criaCartao(
+    'Biologia',
+    'O que são cromossomos homólogos?',
+    'Cromossomos homólogos são pares de cromossomos que apresentam semelhanças estruturais (tamanho e forma), ocupam a mesma posição no cariótipo e carregam genes semelhantes, um de origem materna e outro de origem paterna, que carregam informações genéticas para as mesmas características.');
+    
+    criaCartao(
+    'Física',
+    'O que diz a Primeira lei da Termodinâmica? ',
+    'A Primeira Lei da Termodinâmica se relaciona com o princípio da conservação da energia. Isso quer dizer que a energia em um sistema não pode ser destruída nem criada, somente transformada. A quantidade de calor, o trabalho e a variação de energia interna possuem como unidade de medida padrão o Joule (J).');
+    
+    criaCartao(
+    'Física',
+    'Como passar de km/h para m/s?',
+    'Divida o valor em km/h por 3,6.');
+    
+    criaCartao(
+    'Física',
+    'Qual é a fórmula do Movimento Retilíneo Uniforme?',
+    'S(t) = S₀ + v.t');
+    
+    criaCartao(
+    'Biologia',
+    'Quais são as características dos seres vivos do Reino Monera?',
+    'Os organismos pertencentes ao reino Monera são procariotos e unicelulares que podem formar colônias ou não. Por organismos procariotos, entende-se aqueles que possuem células que não têm núcleo definido, com o material genético disperso pelo citoplasma.');
+    
+    criaCartao(
+    'Matemática',
+    'Qual é a fórmula de uma função afim?',
+    'f(x) = ax + b');
+    
+    criaCartao(
+    'Química',
+    'O que é copolimerização?',
+    'União de várias moléculas idênticas para formar uma nova molécula maior a partir de uma mistura de dois ou vários monómeros .');
+    
+    criaCartao(
+    'História',
+    'Como foi chamado o governo de Getúlio Vargas entre 1937 e 1945? ',
+    'Estado Novo.')
